6e2900ac0b97f27de222da54430cbeee1a0cb80c
chenlongwen <835675949@qq.com>
2020-03-21 23:38:56 +0800

    Initial commit

---
+++ olap_web/src/main/java/cn/doitedu/olap_web/OlapWebApplication.java
@@ -0,0 +1,13 @@
+package cn.doitedu.olap_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlapWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlapWebApplication.class, args);
+    }
+
+}
---
+++ olap_web/src/test/java/cn/doitedu/olap_web/OlapWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.doitedu.olap_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlapWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
